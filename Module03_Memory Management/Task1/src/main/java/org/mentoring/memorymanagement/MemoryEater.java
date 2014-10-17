eadd56399472e2251ca7218e2cacc1b0ba90d909
hhulevich <gulevich.anna@mail.ru>
2014-10-17 21:19:35 +0300

    Task1

---
+++ Module03_Memory Management/Task1/src/main/java/org/mentoring/memorymanagement/MemoryEater.java
@@ -0,0 +1,28 @@
+package org.mentoring.memorymanagement;
+
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: Ivan_Spresov Date: 3/3/14
+ */
+public class MemoryEater {
+	static final Logger logger = Logger.getLogger(MemoryEater.class);
+
+	public static void main(String[] args) {
+		List v = new ArrayList();
+		while (true) {
+			byte b[] = new byte[1048576];
+			v.add(b);
+			Runtime rt = Runtime.getRuntime();
+
+			logger.info("free memory: " + rt.freeMemory());
+			try {
+				Thread.sleep(250);
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+}
