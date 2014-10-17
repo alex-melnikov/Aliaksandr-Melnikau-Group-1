4aa9f710afd1c699e73edaa280eafd6faf9c2fcf
hhulevich <gulevich.anna@mail.ru>
2014-10-17 21:34:31 +0300

    commit

--- Module03_Memory Management/Task1/src/main/java/org/mentoring/memorymanagement/MemoryEater.java
+++ Module03_Memory Management/Task1/src/main/java/org/mentoring/memorymanagement/MemoryEater.java
@@ -6,23 +6,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * User: Ivan_Spresov Date: 3/3/14
+ * User: Ivan_Spresov
+ * Date: 3/3/14
  */
-public class MemoryEater {
-	static final Logger logger = Logger.getLogger(MemoryEater.class);
+public class MemoryEater  {
+    static final Logger logger = Logger.getLogger(MemoryEater.class);
 
-	public static void main(String[] args) {
-		List v = new ArrayList();
-		while (true) {
-			byte b[] = new byte[1048576];
-			v.add(b);
-			Runtime rt = Runtime.getRuntime();
+    public static void main(String[] args) {
+        List v = new ArrayList();
+        while (true) {
+            byte b[] = new byte[1048576];
+            v.add(b);
+            Runtime rt = Runtime.getRuntime();
 
-			logger.info("free memory: " + rt.freeMemory());
-			try {
-				Thread.sleep(250);
-			} catch (InterruptedException e) {
-			}
-		}
-	}
+            System.out.println("free memory: " + rt.freeMemory());
+            /*try {
+                Thread.sleep(250);
+            } catch (InterruptedException e) {
+            }*/
+        }
+    }
 }
